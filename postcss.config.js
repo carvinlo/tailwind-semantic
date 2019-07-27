1401ecef58a0f98f600521330544b0be13e9cbe4
amu <carvin_lo@163.com>
2019-07-27 20:35:41 +0800

    只对 tailwind 进行 purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,6 @@
+/**
+ * 整合到其他项目构建流中，限制在只对 tailwind 进行 purgecss https://github.com/tailwindcss/docs/issues/168
+ */
 const purgecss = require('@fullhuman/postcss-purgecss')({
   content: ['./public/**/*.html'],
   defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
